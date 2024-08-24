2a8fd9b61a81ff7a6b42fd7d99406f1b2755af10
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-08-24 16:42:02 +0700

    Create user request

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateUser } from '@/apis/Api'
 import { AppState, Auth0Provider, User } from '@auth0/auth0-react'
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateUser()
   const domain = import.meta.env.VITE_AUTH0_DOMAIN
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
@@ -14,7 +16,12 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log('User: ', user)
+    if (user?.sub && user?.email) {
+      createUser({
+        auth0Id: user.sub,
+        email: user.email
+      })
+    }
   }
 
   return (
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,22 @@ import App from './App.tsx'
 import './global.css'
 import { BrowserRouter } from 'react-router-dom'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Auth0ProviderWithNavigate>
-      <App />
-    </Auth0ProviderWithNavigate>
+    <QueryClientProvider client={queryClient}>
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
+    </QueryClientProvider>
   </BrowserRouter>
 )

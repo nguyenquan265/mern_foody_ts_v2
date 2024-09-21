dc04bb1003411df3c8083c94d1a807e7fb33a3ed
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-09-21 20:15:44 +0700

    searchbar component

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,77 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormControl, FormField, FormItem } from './ui/form'
+import { Search } from 'lucide-react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+const formSchema = z.object({
+  searchQuery: z.string({
+    required_error: 'Restaurant name is required'
+  })
+})
+
+export type SearchFormData = z.infer<typeof formSchema>
+
+type Props = {
+  onSubmit: (formData: SearchFormData) => void
+  placeholder: string
+  onReset?: () => void
+}
+
+const SearchBar = ({ onSubmit, placeholder, onReset }: Props) => {
+  const form = useForm<SearchFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { searchQuery: '' }
+  })
+
+  const handleReset = () => {
+    form.reset({
+      searchQuery: ''
+    })
+
+    if (onReset) {
+      onReset()
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={`flex items-center flex-1 gap-3 justify-between flex-row border-2 rounded-full p-3 mx-5 ${form.formState.errors.searchQuery && 'border-red-500'}`}
+      >
+        <Search strokeWidth={2.5} size={30} className='ml-1 text-orange-500 hidden md:block' />
+
+        <FormField
+          control={form.control}
+          name='searchQuery'
+          render={({ field }) => (
+            <FormItem className='flex-1'>
+              <FormControl>
+                <Input
+                  {...field}
+                  className='border-none shadow-none focus-visible:ring-0 text-xl'
+                  placeholder={placeholder}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        {form.formState.isDirty && ( // form.formState.isDirty kiem tra xem form da bi thay doi chua
+          <Button onClick={handleReset} type='button' variant='outline' className='rounded-full'>
+            Clear
+          </Button>
+        )}
+
+        <Button type='submit' className='rounded-full bg-orange-500'>
+          Search
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+export default SearchBar
---
+++ client/src/pages/SearchPage.tsx
@@ -0,0 +1,9 @@
+import { useParams } from 'react-router-dom'
+
+const SearchPage = () => {
+  const { city } = useParams()
+
+  return <span>{city}</span>
+}
+
+export default SearchPage

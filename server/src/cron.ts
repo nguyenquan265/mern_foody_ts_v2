2594520f066b48eb306fca95a333787f1de301f3
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-10-03 23:20:40 +0700

    cron 1.1

--- server/src/cron.ts
+++ server/src/cron.ts
@@ -1,15 +1,20 @@
 import { CronJob } from 'cron'
-import https from 'https'
-import http from 'http'
+import https, { RequestOptions } from 'https'
+import http, { RequestOptions as HttpRequestOptions } from 'http'
 import env from './config/env'
 
-const URL = 'https://mern-foody-ts-v2-backend.onrender.com/api/v1/check'
 let job: CronJob
 
 if (env.nodeEnv === 'production') {
   job = new CronJob('*/14 * * * *', function () {
+    const options: RequestOptions = {
+      method: 'GET',
+      hostname: 'mern-foody-ts-v2-backend.onrender.com',
+      path: '/api/v1/check'
+    }
+
     https
-      .get(URL, (res) => {
+      .request(options, (res) => {
         if (res.statusCode === 200) {
           console.log('GET request sent successfully')
         } else {
@@ -19,11 +24,18 @@ if (env.nodeEnv === 'production') {
       .on('error', (e) => {
         console.error('Error while sending request', e)
       })
+      .end()
   })
 } else {
   job = new CronJob('*/14 * * * *', function () {
+    const options: HttpRequestOptions = {
+      method: 'GET',
+      hostname: 'mern-foody-ts-v2-backend.onrender.com',
+      path: '/api/v1/check'
+    }
+
     http
-      .get(URL, (res) => {
+      .request(options, (res) => {
         if (res.statusCode === 200) {
           console.log('GET request sent successfully')
         } else {
@@ -33,6 +45,7 @@ if (env.nodeEnv === 'production') {
       .on('error', (e) => {
         console.error('Error while sending request', e)
       })
+      .end()
   })
 }
 
